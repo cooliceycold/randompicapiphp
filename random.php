66e5e33b0c144d5d0ba8dfb4b7da6d24fdfc8685
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2023-10-19 14:11:38 +0800

    Update random.php

--- random.php
+++ random.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
-	Copyright 2022 梓宸.
-	Blog:https://zichen.zone/
-	Licensed under MIT (https://github.com/zichenace/PHP-Random-Pic-API/blob/main/LICENSE)
-	Ver:1.2.0.20220828
-*/
-
 #配置项目
 $APIname='ZiC API';		//API名称自定义
 $image_file='./img.txt';	//API数据文件
@@ -30,19 +23,19 @@ switch ($returnType) {
 	default:				//默认情况下，直接返回链接
 	header("Location:" . $imgurl);
 
-	case 'json':				//当检测到参数为json
+/*	case 'json':				//当检测到参数为json
 		$json['API_name'] = $APIname;
 		$json['id'] = $randKey;
 		$json['imgurl'] = $imgurl;
 
-		/*
+		
 		#检测图片状态码（按需开启）
 		$code = curl_init ($imgurl);
 		curl_setopt ($code,CURLOPT_RETURNTRANSFER,1);
 		curl_exec ($code);
 		$httpcode = curl_getinfo ( $code, CURLINFO_HTTP_CODE );
 		curl_close ($code);
-		*/
+		
 
 		$imageInfo = getimagesize($imgurl);
 		$json['width'] = "$imageInfo[0]";
@@ -50,7 +43,7 @@ switch ($returnType) {
 		header('Content-type:text/json');
 		echo json_encode($json,JSON_PRETTY_PRINT);
 		break;
-
+*/
 }
 
 ?>
